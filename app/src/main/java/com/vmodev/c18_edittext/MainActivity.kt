c2ed422feaba1d50696589105ccbf5bc7ac5a712
anhtu031199 <anhtu031199@gmail.com>
2021-09-14 17:52:42 +0700

    14-9-commit

---
+++ app/src/main/java/com/vmodev/c18_edittext/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vmodev.c18_edittext
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
